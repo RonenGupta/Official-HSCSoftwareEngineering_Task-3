a52ed66f5c699dca516862fa5b292c65816cad14
RonenGupta <160193990+RonenGupta@users.noreply.github.com>
2026-05-11 09:33:12 +1000

    Twelfth Commit: Implemented visibility functionality
    Now the user must use login with a proper username and password in order to gain access to the train and test tabs. Initially these are set as False, but after creating an additional function in integration for login_btn, I can toggle visibility.

--- integration.py
+++ integration.py
@@ -4,8 +4,25 @@ import gradio as gr
 with gr.Blocks() as demo:
 
     login = LoginSignUp()
-    train = Train_Tab()   
-    test = Test_Tab()     
+
+    with gr.Tab("Train") as train_tab:
+        train = Train_Tab()
+
+    with gr.Tab("Test") as test_tab:
+        test = Test_Tab()
+
+    train_tab.visible = False
+    test_tab.visible = False     
+
+    def visible_tabs(status, user):
+        visible = bool(user)
+        return gr.update(visible=visible), gr.update(visible=visible)
+    
+    login.login_btn.click(
+        fn=visible_tabs,
+        inputs=[login.login_status, login.current_user],
+        outputs=[train_tab, test_tab]
+    )
 
 if __name__ == "__main__":
     demo.launch()
